6054332e2783a55f8f017aa96b03dcdde4800e78
Johnny Reina <johnny.r.reina@gmail.com>
2020-04-20 02:43:56 -0500

    :rocket:

---
+++ README.md
@@ -0,0 +1,8 @@
+## Mouji
+_Performs a random mouse action on an interval to avoid screen locking_
+
+## Why?
+This script was originally written to help with presentations. After having the presentation computer go to sleep over and over and being unable to change screen lock settings on work computers, I wrote this little script to help everyone out.
+
+## Usage
+Command line options can be found by running `mouji -h`. Just running `mouji` will move the mouse between 1 and 5 pixels every 60 seconds.
---
+++ src/index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+const robot = require("robotjs");
+const { program } = require("commander");
+
+program
+  .name("mouji")
+  .option(
+    "-i, --interval <seconds>",
+    "Specify the number of seconds to wait between a mouse action. Default is 60",
+    (x) => parseInt(x, 10),
+    60
+  )
+  .option(
+    "-b, --bound <px>",
+    "Specify the maximum number of pixels to move for each mouse action. Default is 5",
+    (x) => parseInt(x, 10),
+    5
+  )
+  .option("-v, --verbose", "Enable verbose logging. Disabled by default")
+  .parse();
+
+const { interval, bound, verbose } = program.opts();
+const { height, width } = robot.getScreenSize();
+const clampToScreenHeight = clamp(0, height);
+const clampToScreenWidth = clamp(0, width);
+
+function log(...args) {
+  if (verbose) console.log(new Date().toISOString(), ...args);
+}
+
+log(`Starting up`);
+log(`Detected screen dimensions: ${width}x${height}`);
+log(`Performing random mouse action every ${interval} seconds`);
+log(`Max delta ${bound}px`);
+
+const handleInterval = () => {
+  const deltaX = Math.ceil(Math.random() * bound * randomSign());
+  const deltaY = Math.ceil(Math.random() * bound * randomSign());
+  log(`Generated deltas (x, y) = (${deltaX}, ${deltaY})`);
+  const { x: currX, y: currY } = robot.getMousePos();
+  const x = clampToScreenWidth(currX + deltaX);
+  const y = clampToScreenHeight(currY + deltaY);
+  log(`Moving mouse to (${x}, ${y})`);
+  robot.moveMouse(x, y);
+};
+
+function clamp(min, max) {
+  return function (x) {
+    return x < min ? min : x > max ? max : x;
+  };
+}
+
+function randomSign() {
+  return 0.5 - Math.random() > 0 ? 1 : -1;
+}
+
+setInterval(handleInterval, interval * 1000);
